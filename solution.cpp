16d827d7cf51ca3a05851f2b596522be9ee998a5
xs1zy <xstlate@mail.ru>
2025-09-19 23:41:22 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+    int price;   
+    int count;   
+    int total; 
+    std::cout << "СТОИМОСТИ ТОВАРА" << std::endl;
+    std::cout << std::endl;
+    std::cin >> price;
+    std::cin >> count;
+    total = price * count;
+    std::cout << std::endl;
+    std::cout << "Цена за одно: " << price << " руб." << std::endl;
+    std::cout << "Количество: " << count << " шт." << std::endl;
+    std::cout << "Общая стоимость: " << total << " руб." << std::endl;
+    return 0;
 }

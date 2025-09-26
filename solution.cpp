d2f57bb17c4fbfa9446f43a8d630d93d5c3fd340
xs1zy <xstlate@mail.ru>
2025-09-26 22:51:53 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+using namespace std;
+
 int main() {
-  std::cout << "Куплен товар за 100 рублей" << std::endl;
-  return 0;
+    int n;
+    double sum = 0;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        double num;
+        cin >> num;
+        sum += num;
+    }
+    
+    double average = sum / n;
+    cout << average << endl;
+    
+    return 0;
 }
